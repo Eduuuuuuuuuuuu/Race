606fe5fb98db3143537620dd894dae9025a24d3b
Chen075 <125253999+Chen075@users.noreply.github.com>
2024-01-14 17:52:15 +0100

    Circuit change and wall coloring

--- RACE/race/Physics II - Bullet3D - Handout 06 (1)/ModuleSceneIntro.cpp
+++ RACE/race/Physics II - Bullet3D - Handout 06 (1)/ModuleSceneIntro.cpp
@@ -59,7 +59,7 @@ bool ModuleSceneIntro::Start()
 	cube3->SetPos(42, 1, 2);
 	wall = App->physics->AddBody(Cube(2, 10, 10), 0.0);
 	wall->SetPos(58, 1, 2);
-	cube3 = App->physics->AddBody(Cube(12, 5, 34), 0.0);
+	cube3 = App->physics->AddBody(Cube(14, 5, 34), 0.0);
 	cube3->SetPos(53, 1, 20);
 	cube3 = App->physics->AddBody(Cube(15, 5, 8), 0.0);
 	cube3->SetPos(42, 1, 28);
@@ -67,8 +67,8 @@ bool ModuleSceneIntro::Start()
 	wall->SetPos(27, 1, 28);
 	cube3 = App->physics->AddBody(Cube(8, 5, 20), 0.0);
 	cube3->SetPos(32, 1, 34);
-	cube3 = App->physics->AddBody(Cube(18, 5, 8), 0.0);
-	cube3->SetPos(25, 1, 42);
+	cube3 = App->physics->AddBody(Cube(24, 5, 18), 0.0);
+	cube3->SetPos(25, 1, 45);
 	wall = App->physics->AddBody(Cube(10, 10, 2), 0.0);
 	wall->SetPos(27, 1, 47);
 	cube3 = App->physics->AddBody(Cube(8, 4, 16), 0.0);
@@ -79,12 +79,20 @@ bool ModuleSceneIntro::Start()
 	cube3->SetPos(10, 1, 25);
 	wall = App->physics->AddBody(Cube(2, 10, 6), 0.0);
 	wall->SetPos(1.5, 1, 25);
+	wall = App->physics->AddBody(Cube(6, 10, 2), 0.0);
+	wall->SetPos(4, 1, 20);
 	cube3 = App->physics->AddBody(Cube(8, 3, 20), 0.0);
 	cube3->SetPos(5, 1, 38);
 	cube3 = App->physics->AddBody(Cube(40, 3, 8), 0.0);
 	cube3->SetPos(-10, 1, 52);
-	cube3 = App->physics->AddBody(Cube(8, 3, 40), 0.0);
-	cube3->SetPos(-30, 1, 37);
+	cube3 = App->physics->AddBody(Cube(30, 3, 40), 0.0);
+	cube3->SetPos(-35, 1, 42);
+	wall = App->physics->AddBody(Cube(20, 8, 2), 0.0);
+	wall->SetPos(-15, 1, 47);
+	wall = App->physics->AddBody(Cube(26, 8, 2), 0.0);
+	wall->SetPos(-3, 1, 57);
+	cube3 = App->physics->AddBody(Cube(8, 3, 8), 0.0);
+	cube3->SetPos(-35, 1, 18);
 
 	stadium = App->physics->AddBody(Cube(150, 5, 1), 0.0);
 	stadium->SetPos(10, 2 , 70 );
@@ -159,7 +167,7 @@ update_status ModuleSceneIntro::Update(float dt)
 	q1.color = { 200,200,200 };
 	q1.Render();
 
-	Cube q2(12, 5, 34);
+	Cube q2(14, 5, 34);
 	q2.SetPos(53, 1, 20);
 	q2.color = { 200,200,200 };
 	q2.Render();
@@ -174,8 +182,8 @@ update_status ModuleSceneIntro::Update(float dt)
 	q4.color = { 200,200,200 };
 	q4.Render();
 	
-	Cube q5(18, 5, 8);
-	q5.SetPos(25, 1, 42);
+	Cube q5(24, 5, 18);
+	q5.SetPos(25, 1, 45);
 	q5.color = { 200,200,200 };
 	q5.Render();
 	
@@ -199,11 +207,57 @@ update_status ModuleSceneIntro::Update(float dt)
 	q9.color = { 200,200,200 };
 	q9.Render();
 	
-	Cube q10(8, 3, 40);
-	q10.SetPos(-30, 1, 37);
+	Cube q10(30, 3, 40);
+	q10.SetPos(-35, 1, 42);
 	q10.color = { 200,200,200 };
 	q10.Render();
 
+	Cube q11(8, 3, 8);   //finish line
+	q11.SetPos(-35, 1, 18);
+	q11.color = { 200,0,0 };
+	q11.Render();
+
+	//walls
+	Cube e(20, 8, 2);
+	e.SetPos(-15, 1, 47);
+	e.color = { 200,200,0 };
+	e.Render();
+
+	Cube e1(26, 8, 2);
+	e1.SetPos(-3, 1, 57);
+	e1.color = { 200,200,0 };
+	e1.Render();
+	
+	Cube e2(2, 10, 6);
+	e2.SetPos(1.5, 1, 25);
+	e2.color = { 200,200,0 };
+	e2.Render();
+
+	Cube e3(6, 10, 2);
+	e3.SetPos(4, 1, 20);
+	e3.color = { 200,200,0 };
+	e3.Render();
+
+	Cube e4(4, 10, 10);
+	e4.SetPos(11, 1, 34);
+	e4.color = { 200,200,0 };
+	e4.Render();
+	
+	Cube e5(10, 10, 2);
+	e5.SetPos(27, 1, 47);
+	e5.color = { 200,200,0 };
+	e5.Render();
+
+	Cube e6(2, 10, 8);
+	e6.SetPos(27, 1, 28);
+	e6.color = { 200,200,0 };
+	e6.Render();
+	
+	Cube e7(2, 10, 10);
+	e7.SetPos(58, 1, 2);
+	e7.color = { 200,200,0 };
+	e7.Render();
+	//limits border
 	Cube s(150, 5, 1);
 	s.SetPos(10, 2, 70);
 	s.color = { 65,65,65 };
